c84587719c8e5971122687a4510fa54ba756e9ce
magnww <vilosantos@gmail.com>
2023-02-08 13:48:20 +0800

    remove wireguard and wstunnel

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+trap 'exit 0' SIGTERM
+
 SS_ARGS="$@"
 if [ -f ./ss.conf ]; then
   ./$ENTRY server -c ./ss.conf &
@@ -30,20 +32,6 @@ fi
 
 if [ -f ./kcptun_client.conf ]; then
   ./kcptun_client -c ./kcptun_client.conf &
-fi
-
-if [ -f ./wireguard.conf ]; then
-  ip link add dev wg0 type wireguard
-  ip addr add 10.19.19.1/24 dev wg0
-  ip link set mtu 1280 up dev wg0
-  wg setconf wg0 ./wireguard.conf
-  iptables -t nat -A POSTROUTING -s 10.19.19.0/24 -o eth0 -j MASQUERADE
-  iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
-  ./wstunnel -v --server --restrictTo 127.0.0.1:2320 ws://0.0.0.0:2320 &
-fi
-
-if [ -f ./wstunnel.conf ]; then
-  xargs -a wstunnel.conf ./wstunnel &
 fi
 
 if [ -f ./chisel.conf ]; then
